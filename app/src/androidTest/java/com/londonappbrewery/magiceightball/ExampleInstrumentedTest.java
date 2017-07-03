7b43c799868110f6867a5ba2e88de27bd2e416f0
londonappbrewery <londonappbrewery@gmail.com>
2017-07-03 17:25:36 +0100

    fixing error in testing file

--- app/src/androidTest/java/com/londonappbrewery/magiceightball/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/londonappbrewery/magiceightball/ExampleInstrumentedTest.java
@@ -1,26 +1,14 @@
 package com.londonappbrewery.magiceightball;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
+import android.app.Application;
+import android.test.ApplicationTestCase;
 
 /**
- * Instrumentation test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-@RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.londonappbrewery.magiceightball", appContext.getPackageName());
+public class ExampleInstrumentedTest extends ApplicationTestCase<Application> {
+    public ExampleInstrumentedTest() {
+        super(Application.class);
     }
 }
